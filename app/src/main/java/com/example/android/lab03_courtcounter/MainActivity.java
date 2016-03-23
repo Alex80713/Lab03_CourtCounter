7a1ba2c33cd98c5c8513a2f170684928410a5519
aa1991713 <a29763846@gmail.com>
2016-03-23 10:42:29 +0800

    fragment

--- app/src/main/java/com/example/android/lab03_courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/lab03_courtcounter/MainActivity.java
@@ -1,65 +1,47 @@
 package com.example.android.lab03_courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int scoreTeamA;
     private int scoreTeamB;
+
+    private CourtCounterFragment m_fragment_team_a;
+    private CourtCounterFragment m_fragment_team_b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+     protected void onStart() {
+         super.onStart();
+         setFragmentTeamName();
+     }
 
-    public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
-    }
-
-    public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
-    }
-
-    public void add3ForTeamA(View view) {
-        scoreTeamA += 3;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void add2ForTeamA(View view) {
-        scoreTeamA += 2;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void add1ForTeamA(View view) {
-        scoreTeamA += 1;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void add3ForTeamB(View view) {
-        scoreTeamB += 3;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void add2ForTeamB(View view) {
-        scoreTeamB += 2;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void add1ForTeamB(View view) {
-        scoreTeamB += 1;
-        displayForTeamB(scoreTeamB);
+    private void setFragmentTeamName() {
+        m_fragment_team_a =
+                (CourtCounterFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_team_a);
+        m_fragment_team_b =
+                (CourtCounterFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_team_b);
+        m_fragment_team_a.setTeamName("黃蜂");
+        m_fragment_team_b.setTeamName("火箭");
     }
 
     public void resetScoreView(View view) {
-        scoreTeamA = 0;
-        scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+        //scoreTeamA = 0;
+       // scoreTeamB = 0;
 
+       // CourtCounterFragment fragment_team_a =
+       //         (CourtCounterFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_team_a);
+       // CourtCounterFragment fragment_team_b =
+       //         (CourtCounterFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_team_b);
+
+        m_fragment_team_a.resetScore();
+        m_fragment_team_b.resetScore();
     }
+
+
 }
